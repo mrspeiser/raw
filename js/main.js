3d5a45d177d98b764e5b6766a8a947d73611fd4e
Matthew Speiser <mattrspeiser@gmail.com>
2017-08-22 21:07:23 -0700

    navbar good

--- js/main.js
+++ js/main.js
@@ -1,7 +1,26 @@
 (function(){
 
-$('li').hover(function(){
-	$('header').toggleClass('expand');
+$('li').on('mouseover', function(){
+	$('.navigation').addClass('expand');
 });
+
+$('header').on('mouseleave', function(){
+	$('.navigation').removeClass('expand');
+	$('.nav-menu a.active').removeClass('active');
+});
+
+$('.nav-menu a').on('mouseover', function(){
+	$('.nav-menu a.active').removeClass('active');
+	$(this).addClass('active');
+	linkElmnts = $('.nav-menu a').get();
+});
+
+$('.nav-menu a').on('mouseleave', function(){
+	console.log($('.navigation').height() == 0)
+	if ($('.navigation').height() == 0) {
+		$('.nav-menu a.active').removeClass('active');
+	}
+});
+	
 
 })();
